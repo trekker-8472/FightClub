baba93283a490b1abfa5bc3b692238ab548d7487
trekk-8472 <trekker.8472.rp@gmail.com>
2025-09-23 20:07:39 -0400

    Filling the first character

--- main.cpp
+++ main.cpp
@@ -7,10 +7,39 @@
 
 #include <iostream>
 
+#include "Character.h"
+
 using namespace std;
 
 int main() {
-    cout << "project2" << endl;
+    cout << "His Name is Robert Paulson" << endl;
+
+    Character c1;
+    Character c2;
+
+    string name;
+    string role;
+    int HP;
+    int atk;
+    int damage;
+    int armor;
+
+    cout << "Welcome to Fight Club! shhhhh. Name your First Character:";
+    cin >> name;
+    cout << "\n Class:";
+    cin >> role;
+    cout << "\n Number of Hit Points:";
+    cin >> HP;
+    cout << "\n Number for Atk Bonus:";
+    cin >> atk;
+    cout << "\n Amount of Bonus Damage:";
+    cin >> damage;
+    cout << "\n And Finally the Armor Class:";
+    cin >> armor;
+    cin.ignore();
+
+    Character c1(name, role, HP, atk, damage, armor);
+
 
     return 0;
 }
